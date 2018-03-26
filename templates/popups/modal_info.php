cee39706daa575903f8fb13699d39698f9375346
benxander <rguevarac@hotmail.es>
2018-03-25 20:37:04 -0500

    modal dinamico

--- templates/popups/modal_info.php
+++ templates/popups/modal_info.php
@@ -1,11 +1,10 @@
 <div class="modal-header">
-	<h4 class="modal-title"> Where's my code?  </h4>
+	<h4 class="modal-title"> {{seccionWeb[8].contenedor[0].titulo}}  </h4>
 </div>
 <div class="modal-body">
 	<div class="row">
 		<div class="col-sm-12" >
-			<p>If you purchased a digital copy of your photo and video, you'll find your code on your receipt. If you didin't purchased yet, your code is on your bracelet of the excursion.
-			<strong>Need more help?</strong> Visit our FAQs.</p>
+			<div ng-bind-html="seccionWeb[8].contenedor[0].contenido"></div>
 		</div>
 	</div>
 </div>
